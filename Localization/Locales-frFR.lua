58d7ef530df00e46401da4983d8673c1d9d6df95
Shenton <shenton@hotmail.fr>
2013-10-26 04:43:02 +0200

    Localization test

---
+++ Localization/Locales-frFR.lua
@@ -0,0 +1,13 @@
+﻿--[[-------------------------------------------------------------------------------
+    Pets & Mounts
+    Auto and random summon highly customizable for your pets and mounts, with Data Broker support.
+    By: Shenton
+
+    Localization-frFR.lua
+-------------------------------------------------------------------------------]]--
+
+local L = LibStub("AceLocale-3.0"):NewLocale("PetsAndMounts", "frFR");
+
+if L then
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="concat")@
+end
